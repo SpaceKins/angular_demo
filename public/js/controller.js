53cf95971152bb34554f286470a971388b23b5e3
Spacekins <spacekins@zoho.com>
2016-09-17 14:59:48 -1000

    added Factories

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,8 +1,14 @@
 angular.module('myApp')
-  .controller('myController',['$scope','mainCharacter',function($scope,mainCharacter){
+  .controller('myController',[
+    '$scope',
+    'mainCharacter',
+    'CharacterVersionFactory',
+    function($scope,mainCharacter,CharacterVersionFactory){
+    //console.log(CharacterVersionFactory);  
     $scope.myFirstName='Steven Me';
     $scope.myModel ='Ready Player One';
     $scope.mainCharacter =mainCharacter;
+    $scope.characterVersion=CharacterVersionFactory;
   }])
 
   /********** Chaining is allowed *************
