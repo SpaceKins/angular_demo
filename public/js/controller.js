8c7f102047449be7b504fa003a02f7e577c4d817
Spacekins <spacekins@zoho.com>
2016-09-17 15:27:50 -1000

    tep12_service

--- public/js/controller.js
+++ public/js/controller.js
@@ -3,12 +3,20 @@ angular.module('myApp')
     '$scope',
     'mainCharacter',
     'CharacterVersionFactory',
-    function($scope,mainCharacter,CharacterVersionFactory){
+    'BookService',
+    function($scope,mainCharacter,CharacterVersionFactory,BookService){
     //console.log(CharacterVersionFactory);  
     $scope.myFirstName='Steven Me';
     $scope.myModel ='Ready Player One';
     $scope.mainCharacter =mainCharacter;
     $scope.characterVersion=CharacterVersionFactory;
+    $scope.BookService=BookService.getBooks();
+
+    console.log(BookService.getBooks());
+
+    console.log(BookService.getBook(2));
+    
+  //  console.log(service instanceof BookService);
   }])
 
   /********** Chaining is allowed *************
---
+++ public/js/services.js
@@ -0,0 +1,29 @@
+angular.module('myApp')
+.service('BookService',BookService);
+
+
+
+function BookService(){
+  this.value="I am a book service!";
+  this.books=[
+  {
+      id:1,
+      title:'Theory of Everything',
+      author:'Stephen Hawkings'
+    },
+    {
+      id:2,
+      title:'Plain, Honest Men',
+      author:'Richard Beeman'      
+    }
+  ]
+}
+
+
+BookService.prototype.getBook=function(id){
+  return this.books.find((book)=>book.id==id);
+}
+
+BookService.prototype.getBooks=function(){
+    return this.books;
+}
