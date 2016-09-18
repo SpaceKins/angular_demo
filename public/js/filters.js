e191e8f8522ebe16bb7a729f04052956777866ab
Spacekins <spacekins@zoho.com>
2016-09-17 18:40:27 -1000

    added change to > 1983

--- public/js/filters.js
+++ public/js/filters.js
@@ -3,6 +3,6 @@ angular.module('myApp')
       return function(movies,year,message){
         console.log(message);
         console.log(arguments);  
-        return movies.filter(movie => movie.year < year);
+        return movies.filter(movie => movie.year > year);
       }
   });
